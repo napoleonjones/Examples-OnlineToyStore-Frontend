fb11c815962df796bead03a935f6e23d8998e301
Napoleon Jones <napoleon.jones@gotnext.io>
2019-04-17 21:26:18 -0400

    Add project files.

---
+++ OnlineToyStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineToyStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
